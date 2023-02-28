5334f3c2316519f4d706c3ba3f4ad6b13e6d9da9
TaThuHa <thuhak2tltnpt@gmail.com>
2023-02-28 14:08:30 +0700

    up

---
+++ src/Khaibaosudungbien.java
@@ -0,0 +1,14 @@
+public class Khaibaosudungbien {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("i = " +i);
+        float f = 20.5f;
+        System.out.println("f = " +f);
+        boolean b = true;
+        System.out.println("b = " +b);
+        char c = 'a';
+        System.out.println("c = " +c);
+        String s = "Hà Nội";
+        System.out.println("s = " +s);
+    }
+}
